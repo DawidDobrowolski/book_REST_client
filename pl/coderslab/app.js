27bd607efc060b1ebbfe471ebd8891c22e7f3ebe
Dawid Dobrowolski <dawid.dobrowolski@domado.pl>
2019-08-21 16:28:31 +0200

    show book details

--- pl/coderslab/app.js
+++ pl/coderslab/app.js
@@ -1,18 +1,47 @@
 var LINK = "http://localhost:8080/books/"
 var div = $("#books")
 
-$.ajax(LINK).done(function (response) {
+getBooks()
+function getBooks() {$.ajax(LINK).done(function (response) {
     response.forEach(function (item) {
         var newDiv = $("<div>")
-        var newButtonDelete = $("<button>").text("Usun").addClass("book"+item.id).addClass("delete")
-        var newButtonEdit = $("<button>").text("Edytuj").addClass("book"+item.id).addClass("edit")
-        newDiv.text(item.title).addClass("book"+item.id).addClass("books")
+        var newH3 = $("<h3>")
+        var newButtonDelete = $("<button>").text("Delete").addClass("book"+item.id).addClass("delete")
+        var newButtonEdit = $("<button>").text("Edit").addClass("book"+item.id).addClass("edit")
+        newDiv.addClass("book"+item.id).addClass("books")
+        newH3.text(item.title).attr('data-click',"false").attr('data-id',item.id)
+        newDiv.append(newH3)
+        newDiv.attr('data-id',item.id)
         newDiv.append(newButtonEdit).append(newButtonDelete).append($("<div>").hide())
         div.append(newDiv)
     })
 })
+}
 
-$("body").on("click",".books",function () {
-    
+
+$("body").on("click", "h3",function (event) {
+    if(event.target.dataset.click==='false'){
+        $.ajax(LINK+event.target.dataset.id).done(function (res) {
+            var newElement =  $("<div>")
+            newElement.append($("<p>").text("Author: "+res.author.firstName+" " + res.author.lastName))
+            newElement.append($("<p>").text("Publisher: "+res.publisher))
+            newElement.append($("<p>").text("ISBN: "+res.isbn))
+            newElement.append($("<p>").text("Type: "+res.type))
+            $(event.target).after(newElement)
+            newElement.css("display","none")
+            newElement.slideDown()
+            event.target.dataset.click= "true"
+        })
+    }else{
+        $(event.target).parent().find("div").slideUp(800)
+        setTimeout(function () {
+            $(event.target).parent().find("div").remove()
+        },800)
+        event.target.dataset.click= "false"
+    }
+
 })
+
+
+
 
