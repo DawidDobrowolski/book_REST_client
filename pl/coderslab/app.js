01d6357e5831bbba65968bfec05f00f756af52d0
Dawid Dobrowolski <dawid.dobrowolski@domado.pl>
2019-08-22 09:55:04 +0200

    delete book

--- pl/coderslab/app.js
+++ pl/coderslab/app.js
@@ -1,19 +1,19 @@
-var LINK = "http://localhost:8080/books/"
-var div = $("#books")
+var LINK = "http://localhost:8080/books/";
+var div = $("#books");
 
-getBooks()
+getBooks();
 function getBooks() {$.ajax(LINK).done(function (response) {
     response.forEach(function (item) {
-        var newDiv = $("<div>")
-        var newH3 = $("<h3>")
-        var newButtonDelete = $("<button>").text("Delete").addClass("book"+item.id).addClass("delete")
-        var newButtonEdit = $("<button>").text("Edit").addClass("book"+item.id).addClass("edit")
-        newDiv.addClass("book"+item.id).addClass("books")
-        newH3.text(item.title).attr('data-click',"false").attr('data-id',item.id)
-        newDiv.append(newH3)
-        newDiv.attr('data-id',item.id)
-        newDiv.append(newButtonEdit).append(newButtonDelete).append($("<div>").hide())
-        div.append(newDiv)
+        var newDiv = $("<div>");
+        var newH3 = $("<h3>");
+        var newButtonDelete = $("<button>").text("Delete").addClass("book"+item.id).addClass("delete");
+        var newButtonEdit = $("<button>").text("Edit").addClass("book"+item.id).addClass("edit");
+        newDiv.addClass("book"+item.id).addClass("books");
+        newH3.text(item.title).attr('data-click',"false").attr('data-id',item.id);
+        newDiv.append(newH3);
+        newDiv.attr('data-id',item.id);
+        newDiv.append(newButtonEdit).append(newButtonDelete).append($("<div>").hide());
+        div.append(newDiv);
     })
 })
 }
@@ -22,24 +22,38 @@ function getBooks() {$.ajax(LINK).done(function (response) {
 $("body").on("click", "h3",function (event) {
     if(event.target.dataset.click==='false'){
         $.ajax(LINK+event.target.dataset.id).done(function (res) {
-            var newElement =  $("<div>")
-            newElement.append($("<p>").text("Author: "+res.author.firstName+" " + res.author.lastName))
-            newElement.append($("<p>").text("Publisher: "+res.publisher))
-            newElement.append($("<p>").text("ISBN: "+res.isbn))
-            newElement.append($("<p>").text("Type: "+res.type))
-            $(event.target).after(newElement)
-            newElement.css("display","none")
-            newElement.slideDown()
-            event.target.dataset.click= "true"
+            var newElement =  $("<div>");
+            newElement.append($("<p>").text("Author: "+res.author.firstName+" " + res.author.lastName));
+            newElement.append($("<p>").text("Publisher: "+res.publisher));
+            newElement.append($("<p>").text("ISBN: "+res.isbn));
+            newElement.append($("<p>").text("Type: "+res.type));
+            $(event.target).after(newElement);
+            newElement.css("display","none");
+            newElement.slideDown();
+            event.target.dataset.click= "true";
         })
     }else{
-        $(event.target).parent().find("div").slideUp(800)
+        $(event.target).parent().find("div").slideUp(800);
         setTimeout(function () {
             $(event.target).parent().find("div").remove()
-        },800)
-        event.target.dataset.click= "false"
+        },800);
+        event.target.dataset.click= "false";
     }
 
+})
+
+
+$("body").on("click", ".delete",function (event) {
+    if(confirm("Are you sure?")){
+        $(event.target).parent().fadeOut(800);
+        setTimeout(function () {
+            $(event.target).parent().remove()
+        },800);
+        $.ajax({
+            url: LINK+(event.target).closest("div").dataset.id,
+            type: 'DELETE'
+        })
+    }
 })
 
 
